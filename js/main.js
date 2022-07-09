dcc8bc54f36cd286315efc2deb9fe95edc66b5ef
Randy <99023957+Randy-Hwang@users.noreply.github.com>
2022-07-09 13:56:25 +0900

    start to write JS

---
+++ js/main.js
@@ -0,0 +1,60 @@
+(() => {
+  let yOffset = 0; // scrollY 값을 넣을 변수
+
+  const sceneInfo = [
+    {
+      // 스크롤 높이가 길수록 애니메이션 속도가 느리다.
+      // 많이 내려야 하므로!
+      type: "sticky",
+      heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-0"),
+      },
+    },
+    {
+      type: "normal",
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-1"),
+      },
+    },
+    {
+      type: "sticky",
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-2"),
+      },
+    },
+    {
+      type: "sticky",
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-3"),
+      },
+    },
+  ];
+
+  const setLayout = () => {
+    // 각 스크롤 섹션의 높이 세팅
+    for (let i = 0; i < sceneInfo.length; i++) {
+      sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+      sceneInfo[
+        i
+      ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+    }
+  };
+
+  const scrollLoop = () => {};
+
+  addEventListener("resize", setLayout);
+  addEventListener("scroll", () => {
+    yOffset = scrollY;
+    scrollLoop();
+  });
+
+  setLayout();
+})();
